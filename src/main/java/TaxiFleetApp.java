d263da7671bdcbdc0c8f8cf3773fcbe77be5c8bb
Ruslan-Pavlo Maniuk <ruslanmanjuk@gmail.com>
2025-05-13 17:12:37 +0300

    second commit

---
+++ src/main/java/TaxiFleetApp.java
@@ -0,0 +1,13 @@
+
+import gui.MainFrame;
+
+import javax.swing.*;
+
+public class TaxiFleetApp {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MainFrame mainFrame = new MainFrame();
+            mainFrame.setVisible(true);
+        });
+    }
+}
